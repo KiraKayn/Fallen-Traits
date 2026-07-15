8dc7ffdcb3d755afddceeb6e202b778fc3beb692
Kayne <aron.udvardi@gmail.com>
2026-07-15 23:08:18 +0200

    Fixed Creative tab

--- src/main/java/net/kayn/fallen_traits/init/FTCreativeTab.java
+++ src/main/java/net/kayn/fallen_traits/init/FTCreativeTab.java
@@ -14,13 +14,11 @@ public class FTCreativeTab {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FallenTraits.MOD_ID);
 
-    // Define the custom creative tab
     public static final RegistryObject<CreativeModeTab> FALLEN_TRAITS_TAB = CREATIVE_MODE_TABS.register("fallen_traits_tab",
             () -> CreativeModeTab.builder()
-                    .title(Component.translatable("itemGroup.fallen_traits_tab")) // Localization key
-                    .icon(() -> new ItemStack(FTItems.RAGE_GLOVE.get())) // Set the icon of the tab
+                    .title(Component.translatable("itemGroup.fallen_traits_tab"))
+                    .icon(() -> new ItemStack(FTItems.RAGE_GLOVE.get()))
                     .displayItems((parameters, output) -> {
-                        // Add your items here in the order you want them to appear
                         output.accept(FTItems.RAGE_SYMBOL.get());
                         output.accept(FTItems.RAGE_GLOVE.get());
                     })
