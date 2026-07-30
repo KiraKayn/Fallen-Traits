b226958dfd1ff6689beedbabb4f5f4eb407088ed
rtxyd <65117312+rtxyd@users.noreply.github.com>
2026-07-30 21:52:32 +0800

    Remap false

--- src/main/java/net/kayn/fallen_traits/mixin/irons_spellbooks/SpellBarrageGoalMixin.java
+++ src/main/java/net/kayn/fallen_traits/mixin/irons_spellbooks/SpellBarrageGoalMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = SpellBarrageGoal.class)
+@Mixin(value = SpellBarrageGoal.class, remap = false)
 public abstract class SpellBarrageGoalMixin {
 
     @Final
